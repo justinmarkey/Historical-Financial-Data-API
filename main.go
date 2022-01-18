f4005855e6a4018cc877a81ba0c1d592b09c7d0c
Markey <justinsmarkey@gmail.com>
2022-01-18 02:54:09 -0500

    added equity data endpoint and graphing endpoint

--- main.go
+++ main.go
@@ -2,18 +2,65 @@ package main
 
 import (
 	"fmt"
-	"log"
-	//"threadhandler/getsys"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/guptarohit/asciigraph"
+	"github.com/piquette/finance-go/chart"
+	"github.com/piquette/finance-go/datetime"
+	"github.com/piquette/finance-go/equity"
 )
-func Testfunc (name string) {
-    fmt.Println ("Hello" , name) 
-}
+
 func checkErr(err error) {
-    if err != nil {
-        log.Fatal("Error tripped : ", err)
-    }
+	if err != nil {
+		fmt.Printf("ERROR: %v", err)
+	}
+}
+func graphrequestHandler (c *gin.Context) {
+	ticker := c.Query("ticker")
+	todaymonth := time.Now().Month()
+	todayday := time.Now().Day()
+	todayyear := time.Now().Year()
+
+	chartparams := &chart.Params {
+		Symbol: ticker,
+  		Start: &datetime.Datetime{Month: int(todaymonth), Day: todayday, Year: todayyear-1},
+ 		End: &datetime.Datetime{Month: int(todaymonth), Day: todayday, Year: todayyear},
+  		Interval: datetime.OneDay,
+	}
+	
+	genchart := chart.Get(chartparams)
+
+	data := []float64{}
+	for genchart.Next() {
+		bar := genchart.Bar()
+		float, _ := bar.Close.Round(2).Float64()
+		data = append(data, float)
+	}
+	checkErr(genchart.Err())
+	
+	graph := asciigraph.Plot(data, asciigraph.Height(20))
+
+	
+	c.String(200,"%v",graph)
+}
+
+func equityrequestHandler (c *gin.Context) {
+	tickers := c.QueryArray("tickers")
+	response:= equity.List(tickers)
+	
+	for response.Next() {
+		data := response.Equity()
+		
+		c.IndentedJSON(200, data)
+	}
+	checkErr(response.Err())
 }
 
 func main() {
-    
+
+	router := gin.Default()
+	router.GET("/equity", equityrequestHandler)
+	router.GET("/graph", graphrequestHandler)
+	router.Run()
 }
--- main_test.go
+++ main_test.go
@@ -2,7 +2,7 @@ package main_test
 
 import (
 	"testing"
-	"FinAPI"
 )
 func TestMain (t *testing.T ) {
-	FinAPI.Testfunc("hi")
+	
+}
